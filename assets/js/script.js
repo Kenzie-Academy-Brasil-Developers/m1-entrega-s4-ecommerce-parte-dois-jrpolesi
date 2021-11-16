c9b07e7938a9d2e064bf423b60d5f1c4b1f49c7b
jrpolesi <josericardo.kremer@gmail.com>
2021-11-16 20:52:47 -0300

    Add script

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+populateShowcase()
+
+const addToCartButtons = document.querySelectorAll('span[data-id]')
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener('click', addToCart)
+})
+
+
+function addToCart(e){
+  const productId = e.target.dataset.id
+  let product = {}
+  
+  for(let i = 0; i < products.length; i++){
+    if(products[i].id === productId){
+      product = products[i]
+    }
+  }
+
+  if(!cart.includes(product)){
+    cart.push(product)
+  }
+
+  updateCart()
+}
+
+function handleWithShowCartButton(){
+ const buyCartDiv = document.getElementById('containerCart')
+ buyCartDiv.classList.toggle('hide')
+}
